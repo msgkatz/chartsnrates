68adeadf46f3900095acfcc706948a8cd696150f
msgkatz <msgkatz@icloud.com>
2025-03-10 21:29:26 +0300

    few extra libs

--- androidcmpapp/feature/chartgdx/build.gradle.kts
+++ androidcmpapp/feature/chartgdx/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
 
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.compose.ui.tooling)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.runtime)
     implementation(libs.androidx.compose.material3)
